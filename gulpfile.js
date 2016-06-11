4a5eed738f1a30c98ef0008d7317eb46fcb42f2a
Pouya Kary <k@karyfoundation.org>
2016-06-11 13:44:22 +0430

    commenting the Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
 
-gulp.task('typescript', function( cb ) {
-    exec('tsc', function( err ) {
-        if ( err ) return cb( err ); 
-        cb( ); 
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    var gulp    = require('gulp');
+    var exec    = require('child_process').exec;
+    var wrench  = require('wrench');
+    var util    = require('util');
+
+//
+// ─── TOOLS ──────────────────────────────────────────────────────────────────────
+//
+
+    function shell( command , cb ) {
+        exec(command, function( err ) {
+            if ( err ) return cb( err );
+            cb( );
+        });
+    }
+
+//
+// ─── COMPILING TYPE SCRIPT ──────────────────────────────────────────────────────
+//
+
+    gulp.task('typescript', function( cb ) {
+        shell('tsc', cb);
     });
-});
 
-gulp.task('uglifyjs', ['typescript'], function( cb ) {
-    exec('uglifyjs -m -o binary/core.js binary/core.js', function( err ) {
-        if ( err ) return cb( err );
-        cb( );
+    gulp.task('uglifyjs', ['typescript'], function( cb ) {
+        shell('uglifyjs -m -o binary/core.js binary/core.js', cb);
     });
-});
 
-gulp.task('default', ['uglifyjs']);
+//
+// ─── MAIN ───────────────────────────────────────────────────────────────────────
+//
+
+    gulp.task('default', ['uglifyjs']);
