2640940c15abf89354498b0fefece32524d22704
Pouya Kary <k@karyfoundation.org>
2016-06-11 23:14:10 +0430

    build file ready

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,55 @@
 
     var gulp    = require('gulp');
     var exec    = require('child_process').exec;
-    var wrench  = require('wrench');
     var util    = require('util');
+    var fs      = require('fs-extra');
+    var path    = require('path');
 
 //
 // ─── TOOLS ──────────────────────────────────────────────────────────────────────
 //
 
+    /** Run shell commands easy! */
     function shell( command , cb ) {
         exec(command, function( err ) {
             if ( err ) return cb( err );
             cb( );
         });
+    }
+
+//
+// ─── COPY DIR FILES ─────────────────────────────────────────────────────────────
+//
+
+    /** Copy to binary from dir */
+    function copyToBinaryFromDir( dir ) {
+        console.log(`Copying files from ${dir} to`);
+        fs.readdir( dir , function ( err , files ) {
+            // if error
+            if ( err ) {
+                console.log(`Could not get files from directory ${dir}`);
+            };
+            // if right
+            files.forEach( function ( name ) {
+                copyFile(
+                    getLocalPath( path.join( dir , name ) ),
+                    getLocalPath( path.join( 'binary' , name ) )
+                );
+            });
+        });
+    }
+
+    /** Copy file `A` to `B` */
+    function copyFile( A , B ) {
+        fs.copy( A, B, function ( err ) {
+            if ( err ) return console.error( err );
+            console.log(`--> Copied ${A} to ${B}`);
+        });
+    }
+
+    /** Get Local Path in the current directory */
+    function getLocalPath( adrs ) {
+        return path.join( path.dirname( ) , adrs );
     }
 
 //
@@ -37,7 +74,19 @@
     });
 
 //
+// ─── COPY FILES ─────────────────────────────────────────────────────────────────
+//
+
+    gulp.task('copyfiles', function ( cb ) {
+        copyToBinaryFromDir('media');
+        copyToBinaryFromDir('view');
+        copyToBinaryFromDir('style');
+    })
+
+//
 // ─── MAIN ───────────────────────────────────────────────────────────────────────
 //
 
-    gulp.task('default', ['uglifyjs']);
+    gulp.task('default', ['uglifyjs', 'copyfiles']);
+
+// ────────────────────────────────────────────────────────────────────────────────
