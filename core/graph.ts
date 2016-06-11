67b770f25b83cec9c0182455fa1a6a0a83313852
Pouya Kary <k@karyfoundation.org>
2016-06-12 03:39:58 +0430

    min...

--- core/graph.ts
+++ core/graph.ts
@@ -4,92 +4,26 @@
 //   Author: Pouya Kary <k@karyfoundation.org>
 //
 
-/// <reference path="../dfiles/snapsvg.d.ts" />
-/// <reference path="circle.ts" />
-
+/// <reference path="dot.ts" />
+/// <reference path="view.ts" />
+/// <reference path="constants.ts" />
 
 module KaryGraph {
 
     //
-    // ─── OBJECT INTERFACES ──────────────────────────────────────────────────────────
+    // ─── CREATE NODE ────────────────────────────────────────────────────────────────
     //
 
-        /** Implements a connection */
-        export interface IConnection {
-            CircleId: string;
-            LineToCircle: any;
-        }
-
-        /** Implements a dot object. */
-        export interface IDot {
-            Id: string;
-            SnapCircle: ISnapObject;
-            Inputs: Array<IConnection>;
-            Outputs: Array<IConnection>;
-            X: number;
-            Y: number;
-            NumeberId: number;
-            Label: string;
+        export function CreateNode ( x: number, y: number ) {
+            var circle = new Dot( x, y );
+            Graph[ circle.Id ] = circle; 
         }
 
     //
-    // ─── DOT OBJECT ─────────────────────────────────────────────────────────────────
+    // ─── GENERATE RANDOM NODES ──────────────────────────────────────────────────────
     //
 
-        /** Dot object contation information on the nodes of the graph */
-        export class Dot implements IDot {
-
-            //
-			// ─── GLOBALS ────────────────────────────────────────────────
-			//
-
-                /** For counting how many nodes are created. */
-                static TotalDots : number = 0;
-
-			//
-			// ─── DEFS ───────────────────────────────────────────────────
-			//
+        function GenerateSomeRandomNodes( howManyNodes: number ) {
             
-                /** To be used as the hash key in dictionaries */
-                Id: string;
-                /** For displaying the object */
-                SnapCircle: ISnapObject;
-                /** Keeps the Inputs of the dot */
-                Inputs: Array<IConnection>;
-                /** Keeps the Outputs of the dot */
-                Outputs: Array<IConnection>;
-                /** X Coordinates of the dot */
-                X: number;
-                /** Y Coordintaes of the dot */
-                Y: number;
-                /** Unique number id of the dot */
-                NumeberId: number;
-                /** Optional label of the dot */
-                Label: string;
-
-			//
-			// ─── CONSTRUCTOR ────────────────────────────────────────────
-			//
-
-                constructor ( x: number, y: number ) {
-                    // basic allocation
-                    this.X = x;
-                    this.Y = y;
-                    this.Label = '';
-                    // generating the circle
-                    this.SnapCircle = Circle.Create( x, y );
-                    this.Id = this.SnapCircle.id;
-                    // number id
-                    this.NumeberId = ++Dot.TotalDots;
-                    // inputs and outputs
-                    this.Inputs = new Array<IConnection> ( );
-                    this.Outputs = new Array<IConnection> ( );
-                }
-
-			// ────────────────────────────────────────────────────────────
-
         }
-
-    // ────────────────────────────────────────────────────────────────────────────────
-
-}
+} 
