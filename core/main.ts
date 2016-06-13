3aee108adaea3160356caa93609c32d103541be0
Pouya Kary <k@karyfoundation.org>
2016-06-14 03:52:20 +0430

    better naming

--- core/main.ts
+++ core/main.ts
@@ -1,15 +1,14 @@
 
-
 //
 // Copyright 2016 Kary Foundation, Inc.
 //   Author: Pouya Kary <k@karyfoundation.org>
 //
 
-/// <reference path="view.ts" />
+/// <reference path="editor/view.ts" />
 
 
 module KaryGraph {
-    function main ( ) {
+    export function INIT ( ) {
         InitScreenInformation( );
     }
 }
