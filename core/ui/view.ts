f7e887b0d5bba0f151b2489bcb9c4f1914588858
Pouya Kary <k@karyfoundation.org>
2016-06-14 17:52:23 +0430

    moved...

--- core/ui/view.ts
+++ core/ui/view.ts
@@ -5,7 +5,7 @@
 //   Author: Pouya Kary <k@karyfoundation.org>
 //
 
-/// <reference path="dot.ts" />
+/// <reference path="../editor/dot.ts" />
 /// <reference path="../constants.ts" />
 
 
@@ -15,23 +15,37 @@ module KaryGraph {
     // ─── GRAPH ──────────────────────────────────────────────────────────────────────
     //
 
-        /** Main memory containing the graph. Designed in a MVVC model in mind */
-        export var Graph = new Array<Dot> ( );
+        /** 
+         * Main ***memory*** containing the graph. Designed in a MVVC model in mind 
+         */
+        export var Graph = new Array<Dot>( );
         
     //
     // ─── PAPER ──────────────────────────────────────────────────────────────────────
     //
 
+        /**
+         * The **main** view, a ***Snap SVG Paper*** contatining the whole model.
+         */
         export var GraphView = Snap(`#${ GraphViewId }`);
 
+        /** Width of the graph paper */
         export var GraphWidth: number;
+
+        /** Height of the graph paper */
         export var GraphHeight: number;
+
+        /** Graphs distance to the window top */
         export var GraphMarginTop: number;
 
 	//
 	// ─── INIT WINDOW SIZES ──────────────────────────────────────────────────────────
 	//
 
+        /** 
+         * At the *start* of the windw, this function initilizes the window 
+         * properties.
+         */
         export function InitScreenInformation ( ) {
             // getting the paper...
             var paperObject = document.getElementById( GraphViewId );
@@ -41,6 +55,10 @@ module KaryGraph {
             GraphHeight = parseInt( paperStyle.getPropertyValue('height') );
             GraphMarginTop = paperObject.getBoundingClientRect().top;
         }
+
+	//
+	// ─── SCREEN CLICK EVENTS ────────────────────────────────────────────────────────
+	//
 
     // ────────────────────────────────────────────────────────────────────────────────
     
