70fa7f24c0fd3d6413dd48c77e15d479d5c5022c
Pouya Kary <k@karyfoundation.org>
2016-06-14 17:52:08 +0430

    tools for SVG that you don't have on the Snap

---
+++ core/editor/svg.ts
@@ -0,0 +1,20 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+module KaryGraph {
+
+    //
+	// ─── ADD EVENT TO SVG ───────────────────────────────────────────────────────────
+	//
+
+        /** Adds an ***event*** to the ***element*** */
+        export function AddEventOnClick ( element: ISnapObject, event: string ) {
+            document.getElementById( element.id ).setAttribute( 'onClick', event ); 
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────         
+
+}
