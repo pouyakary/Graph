7f3f1d030c39da547d4ce39748c2d3f0ddc5440e
Pouya Kary <k@karyfoundation.org>
2016-06-15 15:08:36 +0430

    It's good to know our project size...

---
+++ loc.sh
@@ -0,0 +1,24 @@
+
+#
+# Copyright 2016 Kary Foundation, Inc.
+#   Author: Pouya Kary <k@karyfoundation.org>
+#
+
+# if you ever wanted to know the project size...
+
+echo "
+//
+// ─── CORE ──────────────────────────────────────────────────────
+//"
+
+sloc ./core 
+
+echo "//
+// ─── SHEETS ────────────────────────────────────────────────────
+//"
+sloc ./sheets 
+
+echo "//
+// ─── VIEW ──────────────────────────────────────────────────────
+//"
+sloc ./view
