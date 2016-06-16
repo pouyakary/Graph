95975016cfbce6f5c0bdd65f84618a7b41f46636
Pouya Kary <k@karyfoundation.org>
2016-06-17 01:09:30 +0430

    best numbering...

--- core/main.ts
+++ core/main.ts
@@ -15,8 +15,8 @@ module KaryGraph {
     /** What starts the code when it starts. */
     export function INIT ( ) {
         InitScreenInformation( );
-        GenerateSomeRandomNodes( 30 );
-        AddRandomConnections( 50 );
+        GenerateSomeRandomNodes( 20 );
+        AddRandomConnections( 20 );
 
         // var selection = new SelectionBox( 800, 100 );
     }
