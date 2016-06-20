19a8db5a32ea6e94c0f588cc0b5d989c293ee35c
Pouya Kary <k@karyfoundation.org>
2016-06-20 15:25:22 +0430

    say implementations is a new way of the console API

--- core/ui/programmer/say.ts
+++ core/ui/programmer/say.ts
@@ -4,6 +4,12 @@
 //   Author: Pouya Kary <k@karyfoundation.org>
 //
 
+/**
+ * Programmer's ***Notebook View*** must have different ways to 
+ * show that viewthis namespace implements different systems for 
+ * the say.
+ */
+
 module KaryGraph.UI.Programmer.SayImplementations {
 
     
