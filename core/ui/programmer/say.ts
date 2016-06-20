52fdd3600357cff3461604f31d2885d57e868035
Pouya Kary <k@karyfoundation.org>
2016-06-20 21:37:11 +0430

    adding the first say implementations

--- core/ui/programmer/say.ts
+++ core/ui/programmer/say.ts
@@ -10,8 +10,64 @@
  * the say.
  */
 
+
+module KaryGraph.UI.Programmer {
+
+    //
+	// ─── SAY MAIN ───────────────────────────────────────────────────────────────────
+	//
+
+        export function Say ( input: any ): string {
+            
+            // if were going to have an undefined type.
+            if ( input == undefined ) {
+                return SayImplementations.Undefined( );
+            }
+
+            switch ( input.constructor.name ) {
+                case "String":
+                    SayImplementations.Text( <string> input );
+                    break;
+
+                case "Object":
+                default:
+                    break;
+            }
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
+
+
+
+
 module KaryGraph.UI.Programmer.SayImplementations {
 
-    
+    //
+	// ─── UNDEFINED ──────────────────────────────────────────────────────────────────
+	//
+
+        export function Undefined ( ): string {
+            return '<div>:D</div>'
+        }
+
+    //
+	// ─── SAY TEXT ───────────────────────────────────────────────────────────────────
+	//
+
+        export function Text ( input: string ) {
+            return `<div class="">'${ input }'</div>`;
+        }    
+
+    //
+	// ─── OBJECT ─────────────────────────────────────────────────────────────────────
+	//
+
+        export function Object ( input: Object ) {
+            return `<div class="">${ input.toString() }</div>`;
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
 
 }
