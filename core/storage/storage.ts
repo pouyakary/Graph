0dc3d364d508fde33b413de1127d303f9a813d14
Pouya Kary <k@karyfoundation.org>
2016-06-22 23:04:28 +0430

    commented...

--- core/storage/storage.ts
+++ core/storage/storage.ts
@@ -1,5 +1,24 @@
 
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+/**
+ * This name space is used to store all the Graph's ***model information***.
+ */
 module KaryGraph.Storage {
-    export var Connections: Object;
-    export var Nodes: Object;
+
+    //
+	// ─── STORAGE ────────────────────────────────────────────────────────────────────
+	//
+
+        /** Contains all the vector objects assosiated to their ids. */
+        export var Connections: Object;
+
+        /** Contains all the dot objects assosiated to their ids. */
+        export var Nodes: Object;
+
+    // ────────────────────────────────────────────────────────────────────────────────
+     
 }
