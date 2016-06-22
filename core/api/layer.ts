3c5f57600e4bfe8e32a00cd967fdd2cdc5ee8db2
Pouya Kary <k@karyfoundation.org>
2016-06-22 22:00:06 +0430

    min...

--- core/api/layer.ts
+++ core/api/layer.ts
@@ -66,15 +66,15 @@ module KaryGraph.API.AbstractionLayer {
                 ( <Dot> Graph[ key ] ).Remove( );
             });
         }
-        
+
     //
     // ─── CREATE MATRIX OF GRAPH ────────────────────────────────────────────────────
     //
 
         export function CreateMatrix( ): number[][] {
-            var matrix: number[ ][ ] = new Array( GetCountOfDots( ) );
+            var matrix: number[ ][ ] = new Array( KaryGraph.Dot.TotalDots );
             for ( var m = 0; m < matrix.length; m++ ) {
-                matrix[ m ] = new Array( GetCountOfDots( ) );
+                matrix[ m ] = new Array( KaryGraph.Dot.TotalDots );
                 for ( var n = 0; n < matrix[ m ].length; n++ ) {
                     matrix[ m ][ n ] = + ( GetDotByNumberId( m + 1 ).IsConnectedTo( GetDotByNumberId( n + 1 ) ) );
                 }
