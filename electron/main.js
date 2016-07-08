fad0125cdd97896d799ac52dbbd2692b712cd05c
Pouya Kary <k@karyfoundation.org>
2016-07-08 16:27:50 +0430

    spacing cleanup....

--- electron/main.js
+++ electron/main.js
@@ -11,39 +11,37 @@
 // ─── DEFS ───────────────────────────────────────────────────────────────────────
 //
 
-	const electron = require( 'electron' );
-	const app = electron.app;
-	const BrowserWindow = electron.BrowserWindow;
-	
+    const electron = require( 'electron' );
+    const app = electron.app;
+    const BrowserWindow = electron.BrowserWindow;
+
 //
 // ─── GENERATE MAIN WINDOW ───────────────────────────────────────────────────────
 //
 
-	let mainWindow;
-	
-	function createWindow ( ) {
-		const window_width = 1100;
-		const window_height = 640;
-		mainWindow = new BrowserWindow({ 
-			width: window_width, 	minWidth: window_width - 200, 
-			height: window_height,	minHeight: window_height - 100,
-			backgroundColor: "#EEEEEE"
-		});
+    let mainWindow;
 
-		mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
-		
-		//mainWindow.webContents.openDevTools();
+    function createWindow ( ) {
+        const window_width = 1100;
+        const window_height = 640;
+        mainWindow = new BrowserWindow({ 
+            width: window_width,    minWidth: window_width - 200, 
+            height: window_height,  minHeight: window_height - 100,
+            backgroundColor: "#EEEEEE"
+        });
 
-		mainWindow.on( 'closed' , function( ) {
-			mainWindow = null;
-			app.quit( );
-		});
-	}
-	
+        mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
+
+        mainWindow.on( 'closed' , function( ) {
+            mainWindow = null;
+            app.quit( );
+        });
+    }
+
 //
 // ─── ON READY ───────────────────────────────────────────────────────────────────
 //
 
-	app.on( 'ready' , createWindow );
+    app.on( 'ready' , createWindow );
 
 // ────────────────────────────────────────────────────────────────────────────────
