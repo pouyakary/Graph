72ce7da54998fc5884e2f3e07ccefc1821d4e058
Pouya Kary <k@karyfoundation.org>
2016-08-15 23:07:55 +0430

    The Kary Horse is now here!

--- core/api/stdlib/kary-foundation.ts
+++ core/api/stdlib/kary-foundation.ts
@@ -13,7 +13,24 @@ module KaryGraph.API.KaryFoundation {
         export function CreateKaryHorseGraph ( ) {
             let height = AbstractionLayer.GetGraphViewHeight( );
             let width = AbstractionLayer.GetGraphViewWidth( );
-            newdotat( width / 2, height / 2  );
+
+            function createDotWithCoordinatesFromOrigin ( x: number, y: number ) {
+                newdotat( ( width / 2 ) + x, ( height / 2 ) + y);
+            }
+
+            createDotWithCoordinatesFromOrigin( -185 , -51  ); // 1
+            createDotWithCoordinatesFromOrigin( -103 , -142 ); // 2
+            createDotWithCoordinatesFromOrigin( -65  ,  5   ); // 3  
+            createDotWithCoordinatesFromOrigin(  118 ,  5   ); // 4
+            createDotWithCoordinatesFromOrigin(  161 ,  -44 ); // 5
+            createDotWithCoordinatesFromOrigin( -139 ,  135 ); // 6
+            createDotWithCoordinatesFromOrigin( -6   ,  135 ); // 7
+            createDotWithCoordinatesFromOrigin(  48  ,  135 ); // 8
+            createDotWithCoordinatesFromOrigin(  181 ,  135 ); // 9
+
+            connect(range(1, 5));
+            connect([6, 3, 7]);
+            connect([8, 4, 9]);
         }
     
     // ────────────────────────────────────────────────────────────────────────────────
