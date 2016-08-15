fd82ed92a2359dcbab148fe71916b5664e5ae867
Pouya Kary <k@karyfoundation.org>
2016-08-15 23:27:37 +0430

    fixing the issues...

--- core/api/stdlib/kary-foundation.ts
+++ core/api/stdlib/kary-foundation.ts
@@ -10,27 +10,38 @@ module KaryGraph.API.KaryFoundation {
     // ─── THE KARY GRAPH ─────────────────────────────────────────────────────────────
     //
 
+        /** 
+         * Pretty fun easter that generates a Kary Horse Logo with exact
+         * properties as the real logo in the center of the screen.
+         */
         export function CreateKaryHorseGraph ( ) {
-            let height = AbstractionLayer.GetGraphViewHeight( );
-            let width = AbstractionLayer.GetGraphViewWidth( );
+            // screen sizes
+            const height = AbstractionLayer.GetGraphViewHeight( );
+            const width = AbstractionLayer.GetGraphViewWidth( );
 
-            function createDotWithCoordinatesFromOrigin ( x: number, y: number ) {
-                newdotat( ( width / 2 ) + x, ( height / 2 ) + y);
+            /** Creates a function  */
+            function createFromOrigin ( x: number, y: number ): Dot {
+                return newdotat( ( width / 2 ) + x, ( height / 2 ) + y);
             }
 
-            createDotWithCoordinatesFromOrigin( -185 , -51  ); // 1
-            createDotWithCoordinatesFromOrigin( -103 , -142 ); // 2
-            createDotWithCoordinatesFromOrigin( -65  ,  5   ); // 3  
-            createDotWithCoordinatesFromOrigin(  118 ,  5   ); // 4
-            createDotWithCoordinatesFromOrigin(  161 ,  -44 ); // 5
-            createDotWithCoordinatesFromOrigin( -139 ,  135 ); // 6
-            createDotWithCoordinatesFromOrigin( -6   ,  135 ); // 7
-            createDotWithCoordinatesFromOrigin(  48  ,  135 ); // 8
-            createDotWithCoordinatesFromOrigin(  181 ,  135 ); // 9
+            /** Where the dots are saved. */
+            let dots = new Array<Dot>( 9 );
 
-            connect(range(1, 5));
-            connect([6, 3, 7]);
-            connect([8, 4, 9]);
+            // Creating dots
+            dots[ 0 ] = createFromOrigin( -185 , -51  ); // 1
+            dots[ 1 ] = createFromOrigin( -103 , -142 ); // 2
+            dots[ 2 ] = createFromOrigin( -65  ,  5   ); // 3  
+            dots[ 3 ] = createFromOrigin(  118 ,  5   ); // 4
+            dots[ 4 ] = createFromOrigin(  161 ,  -44 ); // 5
+            dots[ 5 ] = createFromOrigin( -139 ,  135 ); // 6
+            dots[ 6 ] = createFromOrigin( -6   ,  135 ); // 7
+            dots[ 7 ] = createFromOrigin(  48  ,  135 ); // 8
+            dots[ 8 ] = createFromOrigin(  181 ,  135 ); // 9
+
+            // Connecting
+            connect([ dots[ 0 ], dots[ 1 ], dots[ 2 ], dots[ 3 ], dots[ 4 ] ]);
+            connect([ dots[ 5 ], dots[ 2 ], dots[ 6 ]]);
+            connect([ dots[ 7 ], dots[ 3 ], dots[ 8 ]]);
         }
     
     // ────────────────────────────────────────────────────────────────────────────────
