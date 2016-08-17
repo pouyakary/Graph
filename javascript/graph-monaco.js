0eb36a11b2a470085a3216743851be47b3e9a87c
Pouya Kary <k@karyfoundation.org>
2016-08-18 00:31:46 +0430

    cu

--- javascript/graph-monaco.js
+++ javascript/graph-monaco.js
@@ -9,22 +9,28 @@
 //
 
     function GraphInitMonacoOnStart ( ) {
-        function uriFromPath(_path) {
-            var pathName = path.resolve(_path).replace(/\\/g, '/');
-            if (pathName.length > 0 && pathName.charAt(0) !== '/') {
+
+        function uriFromPath ( _path ) {
+            var pathName = path.resolve( _path ).replace( /\\/g, '/' );
+            if ( pathName.length > 0 && pathName.charAt( 0 ) !== '/' ) {
                 pathName = '/' + pathName;
             }
-            return encodeURI('file://' + pathName);
+            return encodeURI( 'file://' + pathName );
         }
+
         require.config({
-            baseUrl: uriFromPath(path.join(__dirname, '../node_modules/monaco-editor/min'))
+            baseUrl: uriFromPath(
+                path.join( __dirname, '../node_modules/monaco-editor/min' ) )
         });
+
         // workaround monaco-css not understanding the environment
         self.module = undefined;
+
         // workaround monaco-typescript not understanding the environment
         self.process.browser = true;
-        require(['vs/editor/editor.main'], function() {
-            var editor = monaco.editor.create(document.getElementById('monaco-placeholder'), {
+
+        require([ 'vs/editor/editor.main' ], function ( ) {
+            var editor = monaco.editor.create(document.getElementById( 'monaco-placeholder' ), {
                 value: [
                     '',
                     '// We have a very awesome wiki about the graph API here:',
@@ -34,7 +40,7 @@
                     'reset( );',
                     'let dots = newdots( 10 );',
                     'connect( range( 1, 10 ) );',
-                ].join('\n'),
+                ].join( '\n' ),
                 language: 'javascript',
                 fontFamily: 'GraphSourceCodePro',
                 fontSize: 13,
