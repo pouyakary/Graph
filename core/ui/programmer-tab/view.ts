e815474a39b6d05468df7b79db6378972f8b784e
Pouya Kary <k@karyfoundation.org>
2016-08-20 01:25:57 +0430

    a much better loading experience...

--- core/ui/programmer-tab/view.ts
+++ core/ui/programmer-tab/view.ts
@@ -18,6 +18,56 @@ module KaryGraph.UI.ProgrammerTab {
 
         export function ApplyAdditionalMonacoInitialization ( ) {
             KaryFoundation.Monaco.AddSupportingMonacoTools( Editor );
+            SetSampleCode( );
+        }
+
+    //
+    // ─── GENERATE SAMPLE CODE ───────────────────────────────────────────────────────
+    //
+
+        function SetSampleCode ( ) {
+            let sampleCode = [
+                '',
+                generateSectionComment('welcome'),
+                ' ',
+                '    // Hello there! Welcome to Graph!',
+                '    // it really is easy to code for graph, all you need is some',
+                '    // sweet portion of javascript and a one time checking out at',
+                '    // this API documentation:',
+                '    // https://github.com/karyfoundation/graph/wiki/API',
+                '',
+                '    let size = 10;',
+                '    newdots( size );',
+                '    for ( let i = 0; i < size; i++ ) {',
+                '        connect([ rnd( size ), rnd( size ) ]);',
+                '    }',
+                '',
+                generateLineComment( )
+            ].join('\n');
+
+            Editor.setValue( sampleCode );
+        }
+
+    //
+    // ─── GENERATE SECTION COMMENT ───────────────────────────────────────────────────
+    //
+
+        function generateSectionComment ( text: string ) {
+            return KaryFoundation.Comment.Generate(
+                KaryFoundation.Comment.Style.Section,
+                '//', 4, true, text
+            ).trim( );
+        }
+
+    //
+    // ─── GENERATE LINE COMMENT ──────────────────────────────────────────────────────
+    //
+
+        function generateLineComment ( ) {
+            return KaryFoundation.Comment.Generate(
+                KaryFoundation.Comment.Style.Ending,
+                '//', 4, true, ''
+            )
         }
 
     // ────────────────────────────────────────────────────────────────────────────────
--- javascript/graph-monaco.js
+++ javascript/graph-monaco.js
@@ -31,17 +31,7 @@
         require([ 'vs/editor/editor.main' ], function ( ) {
             KaryGraph.UI.ProgrammerTab.Editor = monaco.editor.create(
                 document.getElementById( 'monaco-placeholder' ), {
-                    value: [
-                        '',
-                        '// We have a very awesome wiki about the graph API here:',
-                        '// https://github.com/karyfoundation/graph/wiki/API',
-                        "// we're sure you'll love it...",
-                        '',
-                        'reset( );',
-                        'let dots = newdots( 10 );',
-                        'connect( range( 1, 10 ) );',
-                        ''
-                    ].join( '\n' ),
+                    value: '',
                     language: 'javascript',
                     fontFamily: 'GraphSourceCodePro',
                     fontSize: 13,
