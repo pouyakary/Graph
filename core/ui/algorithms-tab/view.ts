0e60c551f6cd42e4f055083c2760810dd3666ad6
Pouya Kary <k@karyfoundation.org>
2016-08-23 21:45:34 +0430

    tools for generating the view

--- core/ui/algorithms-tab/view.ts
+++ core/ui/algorithms-tab/view.ts
@@ -10,8 +10,42 @@ namespace KaryGraph.UI.AlgorithmsTab {
     // ─── MAKE AN ALGORITHM CONTROL VIEW ─────────────────────────────────────────────
     //
 
-        export function MakeAlgorithmControlView ( ) {
+        export function MakeAlgorithmControlView (
+            algorithmObject: ScriptEngine.Algorithms.IAlgorithmObject ) {
+            let view = MakeViewForAlgorithm( algorithmObject );
+            document.getElementById( AlgorithmsControlContainerId ).appendChild( view );
+        }
 
+    //
+    // ─── GET CONTROLLER ID ──────────────────────────────────────────────────────────
+    //
+
+        export function GetControllerId ( handle: string ) {
+            return 'algorithm-box-' + handle;
+        }
+
+    //
+    // ─── MAKE ALGORITHM VIEW ELEMENTS ───────────────────────────────────────────────
+    //
+
+        function MakeViewForAlgorithm (
+            algorithmObject: ScriptEngine.Algorithms.IAlgorithmObject ) {
+
+            let controller = document.createElement('div');
+            controller.className = AlgorithmsControllerClass;
+            controller.id = GetControllerId( algorithmObject.manifest.handle );
+
+                let title = document.createElement('div');
+                title.className = AlgorithmsControllerTitleClass;
+                title.innerText = algorithmObject.manifest.name;
+                controller.appendChild( title );
+
+                let author = document.createElement('div');
+                author.className = AlgorithmsControllerAuthorClass;
+                author.innerText = `By ${ algorithmObject.manifest.author }`;
+                controller.appendChild( author );
+
+            return controller;
         }
 
     // ────────────────────────────────────────────────────────────────────────────────
